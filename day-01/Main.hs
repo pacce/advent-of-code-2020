4bf28bfe0c21bf38573c5790eff947964e636049
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-03 02:24:40 -0300

    feat: adds input reader

--- day-01/Main.hs
+++ day-01/Main.hs
@@ -1,4 +1,9 @@
 module Main where
+import Control.Monad
+
+readLines :: FilePath -> IO [Int]
+readLines = fmap rd . readFile
+        where rd x = map (read :: String -> Int) (lines x)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
