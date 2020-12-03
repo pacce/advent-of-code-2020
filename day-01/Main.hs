0828503e8d9799dd7ca41330588e9e34092bafa0
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-03 03:43:07 -0300

    feat: adds pair finder and product function

--- day-01/Main.hs
+++ day-01/Main.hs
@@ -1,9 +1,14 @@
 module Main where
-import Control.Monad
+import Data.List
 
 readLines :: FilePath -> IO [Int]
-readLines = fmap rd . readFile
-        where rd x = map (read :: String -> Int) (lines x)
+readLines = fmap (fmap read) . (fmap lines . readFile)
+
+findPairs :: [Int] -> Int -> [(Int, Int)]
+findPairs xs yr = [(x, y) | x <- xs, y <- xs, x + y == yr]
+
+solvePuzzle :: [Int] -> Int -> [Int]
+solvePuzzle xs yr = nub $ map (\(x, y) -> x * y) (findPairs xs yr)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
