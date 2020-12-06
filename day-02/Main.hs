e37b441da76893df23f58537b313415d45ec761d
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-06 00:12:34 -0300

    feat: solves part 2 of puzzle

--- day-02/Main.hs
+++ day-02/Main.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Data.List
 import Text.Parsec
 import Text.Parsec.Text
 
@@ -33,11 +34,17 @@ valid :: Policy -> Bool
 valid p = (low p) <= count && count <= (high p)
     where count = length $ filter (\c -> key p == c) (password p)
 
+valid2 :: Policy -> Bool -- second puzzle
+valid2 p = (length $ filter pos vs) == 1
+    where vs = map (+1) (elemIndices (key p) (password p))
+          pos v = v == (low p) || v == (high p)
+
 main :: IO ()
 main = do
     { result <- parseFromFile policies "./input/input.txt"
     ; case result of
         Left err -> print err
-        Right ps -> print $ length (filter valid ps)
+        Right ps
+            -> (print $ length (filter valid ps))
+            >> (print $ length (filter valid2 ps))
     }
-
