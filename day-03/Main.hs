51cf410e33326fa37d514a4a95ed35feaacbf09b
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-06 01:26:48 -0300

    feat: solves day 3 of puzzle

--- day-03/Main.hs
+++ day-03/Main.hs
@@ -1,4 +1,39 @@
 module Main where
 
+import Data.List
+
+type Coordinate = Int
+type Count      = Int
+type Line       = [Int]
+type Step       = Int
+type Width      = Int
+
+data Map = Map {
+    mapLines    :: [Line],
+    mapWidth    :: Width
+} deriving (Show)
+
+createMap :: [String] -> Map
+createMap ss = Map (map (elemIndices '#') ss) (length $ head ss)
+
+step :: Width -> Step -> Coordinate -> Coordinate
+step w s c = (c + s) `mod` w
+
+hit :: Coordinate -> Line -> Count
+hit y ls = (length $ elemIndices y ls)
+
+hits :: [Line] -> (Coordinate -> Coordinate) -> Coordinate -> Count -> Count
+hits [] _ _ count = count
+hits (l:ls) stepper curr count = hits ls stepper next updated
+    where next      = stepper curr
+          updated   = count + (hit curr l)
+
+solve :: Map -> Step -> Count
+solve m s = hits (mapLines m) stepper start 0
+    where stepper   = step (mapWidth m) s
+          start     = 0
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    xs <- fmap lines (readFile "input/input.txt")
+    print $ solve (createMap xs) 3
