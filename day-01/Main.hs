1fdc88404c7186005c2ab401c93217f533707a34
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-06 22:54:04 -0300

    refactor: solves part 3 and reads input from file

--- day-01/Main.hs
+++ day-01/Main.hs
@@ -8,11 +8,21 @@ readLines = fmap (fmap read) . (fmap lines . readFile)
 findPairs :: [Int] -> Int -> [(Int, Int)]
 findPairs xs yr = [(x, y) | x <- xs, y <- xs, x + y == yr]
 
-solvePuzzle :: [Int] -> Int -> [Int]
-solvePuzzle xs yr = nub $ map (\(x, y) -> x * y) (findPairs xs yr)
+findTriplets :: [Int] -> Int -> [(Int, Int, Int)]
+findTriplets xs yr =
+    [(x, y, z)
+    | x <- xs
+    , y <- xs
+    , z <- xs
+    , x + y + z == yr
+    ]
+
+solvePuzzle :: Int -> [Int] -> Int -> [Int]
+solvePuzzle 2 xs yr = nub $ map (\(x, y) -> x * y) (findPairs xs yr)
+solvePuzzle 3 xs yr = nub $ map (\(x, y, z) -> x * y * z) (findTriplets xs yr)
 
 main :: IO ()
 main = do
-    args    <- getArgs
-    xs      <- readLines $ head args
-    print $ solvePuzzle xs 2020
+    xs <- readLines "input/input.txt"
+    print $ head (solvePuzzle 2 xs 2020)
+    print $ head (solvePuzzle 3 xs 2020)
