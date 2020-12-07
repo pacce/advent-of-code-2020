27a676070f85ee2aceecd7aaad28215e2c6bd025
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-07 01:25:22 -0300

    feat: solves for reference input

--- day-06/Main.hs
+++ day-06/Main.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
+import Data.Set
+import Text.Parsec
+import Text.Parsec.Text
+
+type Person = [Char]
+type Group  = [Person]
+
+person :: Stream s m Char => ParsecT s u m Person
+person = (many1 letter) <* newline
+
+group :: Stream s m Char => ParsecT s u m [Group]
+group = sepBy (many1 person) newline
+
+process :: Group -> Int
+process = length . unions . fmap Data.Set.fromList
+
+solve :: [Group] -> Int
+solve gs = Prelude.foldr (+) 0 (fmap process gs)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    { result <- parseFromFile group "input/reference.txt"
+    ; case result of
+        Left err -> print err
+        Right ps -> print $ solve ps
+    }
--- day-06/shell.nix
+++ day-06/shell.nix
@@ -4,14 +4,14 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, stdenv }:
+  f = { mkDerivation, base, parsec, stdenv }:
       mkDerivation {
         pname = "day06";
         version = "0.1.0.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
-        executableHaskellDepends = [ base ];
+        executableHaskellDepends = [ base parsec ];
         license = "unknown";
         hydraPlatforms = stdenv.lib.platforms.none;
       };
