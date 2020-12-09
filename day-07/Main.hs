c9782db51baf9d82de01d123012502b5c5cda755
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-09 13:38:32 -0300

    feat: sets up day 6 project

---
+++ day-07/Main.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Main where
+
+import Text.Parsec
+import Text.Parsec.Text
+
+type Adjective      = [Char]
+type Bag            = [Char]
+type Color          = [Char]
+type Description    = [Char]
+type Rule           = (Description, [Description])
+type Verb           = [Char]
+
+adjective :: Stream s m Char => ParsecT s u m Adjective
+adjective = many1 letter
+
+bag :: Stream s m Char => ParsecT s u m Bag
+bag = string "bag" <* optional (char 's')
+
+color :: Stream s m Char => ParsecT s u m Color
+color = many1 letter
+
+natural :: Stream s m Char => ParsecT s u m Int
+natural = read <$> many1 digit
+
+verb :: Stream s m Char => ParsecT s u m Verb
+verb = string "contain"
+
+description :: Stream s m Char => ParsecT s u m Description
+description = do
+    { a <- adjective
+    ; _ <- spaces
+    ; c <- color
+    ; _ <- spaces
+    ; _ <- bag
+    ; return (a ++ " " ++ c)
+    }
+
+empty :: Stream s m Char => ParsecT s u m [Description]
+empty = do
+    { _ <- string "no other"
+    ; _ <- spaces
+    ; _ <- bag
+    ; return []
+    }
+
+container :: Stream s m Char => ParsecT s u m Description
+container = description
+
+contained :: Stream s m Char => ParsecT s u m [Description]
+contained = choice
+    [ do { _ <- natural ; _ <- spaces ; x <- description ; return [x] }
+    , empty
+    ]
+
+rule :: Stream s m Char => ParsecT s u m Rule
+rule = do
+    { x     <- container
+    ; _     <- spaces
+    ; _     <- verb
+    ; _     <- spaces
+    ; ys    <- sepBy contained (string ", ")
+    ; _     <- char '.'
+    ; _     <- endOfLine
+    ; return (x, foldr (++) [] ys)
+    }
+
+main :: IO ()
+main = do
+    { result <- parseFromFile (many1 rule) "input/reference.txt"
+    ; case result of
+        Left err -> print err
+        Right ps -> print ps
+    }
