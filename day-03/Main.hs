354a1f98b33629d75a7706f7f9405e6a37f9afd0
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-13 23:50:48 -0300

    refactor!: allows for arbitrary y steps

--- day-03/Main.hs
+++ day-03/Main.hs
@@ -2,38 +2,51 @@ module Main where
 
 import Data.List
 
-type Coordinate = Int
+type Coordinate = (Int, Int)
 type Count      = Int
+type Height     = Int
 type Line       = [Int]
-type Step       = Int
+type Step       = (Int, Int)
 type Width      = Int
+
+type Stepper    = (Coordinate -> Coordinate)
 
 data Map = Map {
     mapLines    :: [Line],
-    mapWidth    :: Width
+    mapWidth    :: Width,
+    mapHeight   :: Height
 } deriving (Show)
 
 createMap :: [String] -> Map
-createMap ss = Map (map (elemIndices '#') ss) (length $ head ss)
+createMap ss = Map (map (elemIndices '#') ss) (length $ head ss) (length ss)
+
+mapComplete :: Map -> Coordinate -> Bool
+mapComplete m cs = snd cs >= mapHeight m
 
 step :: Width -> Step -> Coordinate -> Coordinate
-step w s c = (c + s) `mod` w
+step w (sx, sy) (cx, cy) = ((cx + sx) `mod` w, cy + sy)
 
-hit :: Coordinate -> Line -> Count
-hit y ls = (length $ elemIndices y ls)
+tree :: Map -> Coordinate -> Bool
+tree m (x, y)
+    | mapComplete m (x, y)  = False
+    | otherwise             = (not . null) $ elemIndices x line
+        where line = (mapLines m !! y)
 
-hits :: [Line] -> (Coordinate -> Coordinate) -> Coordinate -> Count -> Count
-hits [] _ _ count = count
-hits (l:ls) stepper curr count = hits ls stepper next updated
-    where next      = stepper curr
-          updated   = count + (hit curr l)
+hit :: Map -> Stepper -> Coordinate -> Count -> Count
+hit m stepper curr count
+    | mapComplete m curr    = count
+    | otherwise             = hit m stepper next updated
+         where next      = stepper curr
+               updated
+                | tree m curr = count + 1
+                | otherwise   = count
 
 solve :: Map -> Step -> Count
-solve m s = hits (mapLines m) stepper start 0
-    where stepper   = step (mapWidth m) s
-          start     = 0
+solve m s = hit m stepper start 0
+     where stepper = step (mapWidth m) s
+           start   = (0, 0)
 
 main :: IO ()
 main = do
     xs <- fmap lines (readFile "input/input.txt")
-    print $ solve (createMap xs) 3
+    print $ solve (createMap xs) (3, 1)
