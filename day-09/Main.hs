9a971f8e94ddb84a67d68d71b74cab851ccd5b16
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-17 12:36:53 -0300

    feat: solves day 9 for reference input

---
+++ day-09/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+import Data.Maybe (isJust)
+
+type Value  = Int
+type Window = [Int]
+
+chunk :: Int -> [Value] -> [(Value, Window)]
+chunk k ys = filter (\(_, v) -> length v == k) (ck k ys)
+    where ck :: Int -> [Value] -> [(Value, Window)]
+          ck k [] = []
+          ck k (x:xs) = [(x, take k xs)] ++ ck k xs
+
+eval :: (Value, Window) -> Maybe Value
+eval (x, ws)
+    | null ys       = Just x
+    | otherwise     = Nothing
+    where ys = [w1 + w2 | w1 <- ws, w2 <- ws, w1 + w2 == x, w1 /= w2]
+
+solve :: Int -> [Value] -> Maybe Value
+solve k xs = head $ filter isJust ys
+    where ys = map eval (chunk k $ reverse xs)
+
+main :: IO ()
+main = do
+    xs <- fmap lines $ readFile "input/reference.txt"
+    print $ solve 5 (fmap read xs)
