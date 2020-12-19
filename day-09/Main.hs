55c4b62f322ad28fc564bde68f65df38f1e59ecd
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2020-12-19 02:45:55 -0300

    feat: naive solution for part 2, reference input

--- day-09/Main.hs
+++ day-09/Main.hs
@@ -16,11 +16,25 @@ eval (x, ws)
     | otherwise     = Nothing
     where ys = [w1 + w2 | w1 <- ws, w2 <- ws, w1 + w2 == x, w1 /= w2]
 
+search :: Int -> [Value] -> Window
+search v vs = head $ filter (\y -> (foldr (+) 0 y) == v) ys
+    where xs = reverse [2..length vs]
+          ys = concat $ map (\x -> map snd $ chunk x vs) xs
+
 solve :: Int -> [Value] -> Maybe Value
 solve k xs = head $ filter isJust ys
     where ys = map eval (chunk k $ reverse xs)
 
+solve2 :: Int -> [Value] -> Value
+solve2 v vs = (maximum xs) + (minimum xs)
+    where xs = search v vs
+
 main :: IO ()
 main = do
-    xs <- fmap lines $ readFile "input/input.txt"
-    print $ solve 25 (fmap read xs)
+    xs <- fmap lines $ readFile "input/reference.txt"
+    let ys = fmap (read::String -> Int) xs
+
+    case solve 5 ys of
+        Just x  -> print x
+                >> (print $ solve2 x ys)
+        Nothing -> print "unsovable"
